f09221f3cb82789e0006211ae3a1eac87ac83137
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-11 19:20:28 +0200

    Move class outside the GameObjectActivate class

--- Editor/GameObjectActivateEditor.cs
+++ Editor/GameObjectActivateEditor.cs
@@ -3,22 +3,22 @@ using System.Collections;
 using UnityEditor;
 using Rotorz.ReorderableList;
 
-namespace OneDayGame {
+namespace GameObjectActivator {
 
 	[CustomEditor(typeof(GameObjectActivate))]
 	public class GameObjectActivateEditor : Editor {
 
-		private SerializedProperty _objsToEnable;
+		private SerializedProperty objsToEnable;
 
 		private void OnEnable() {
-			_objsToEnable = serializedObject.FindProperty("_objsToEnable");
+			objsToEnable = serializedObject.FindProperty("objsToEnable");
 		}
 
 		public override void OnInspectorGUI() {
 			serializedObject.Update();
 
 			ReorderableListGUI.Title("Objects to enable");
-			ReorderableListGUI.ListField(_objsToEnable);
+			ReorderableListGUI.ListField(objsToEnable);
 
 			serializedObject.ApplyModifiedProperties();
 		}
--- GameObjectActivate.cs
+++ GameObjectActivate.cs
@@ -2,70 +2,78 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace OneDayGame {
+namespace GameObjectActivator {
 
-	/// Enable child game object on collision with
-	/// other game object.
-	public class GameObjectActivate : MonoBehaviour {
+    // todo move to file
+    public enum TagOptions { Include, Exclude }
 
-		public enum TagOptions { Include, Exclude }
+    // todo move to file
+    /// Object to enable.
+    [System.Serializable]
+    public class ObjectToEnable {
 
-		/// Object to enable.
-		[System.Serializable]
-		public class ObjectToEnable {
+        /// Game object.
+        [SerializeField]
+        private GameObject _objToEnable;
+        public GameObject ObjToEnable {
+            get { return _objToEnable; }
+            set { _objToEnable = value; }
+        }
 
-			/// Game object.
-			[SerializeField]
-			private GameObject _objToEnable;
-			public GameObject ObjToEnable {
-				get { return _objToEnable; }
-				set { _objToEnable = value; }
-			}
+        /// Select to include or exclude a given tag.
+        [SerializeField]
+        private TagOptions _tagOption;
+        public TagOptions TagOption {
+            get { return _tagOption; }
+        }
 
-			/// Select to include or exclude a given tag.
-			[SerializeField]
-			private TagOptions _tagOption;
-			public TagOptions TagOption {
-				get { return _tagOption; }
-			}
+        /// On collision with objects with this tag
+        /// the object won't be enabled.
+        [SerializeField]
+        private string _tag;
+        public string ExcludeTag {
+            get { return _tag; }
+            set { _tag = value; }
+        }
+    }
 
-			/// On collision with objects with this tag
-			/// the object won't be enabled.
-			[SerializeField]
-			private string _tag;
-			public string ExcludeTag {
-				get { return _tag; }
-				set { _tag = value; }
-			}
-		}
+    /// Enable child game object on collision with
+    /// other game object.
+    public class GameObjectActivate : MonoBehaviour {
 
-		public List<ObjectToEnable> _objsToEnable = new List<ObjectToEnable>();
+        private List<ObjectToEnable> objsToEnable = new List<ObjectToEnable>();
 
-		private void FixedUpdate() {
-		}
+        public List<ObjectToEnable> ObjsToEnable {
+            get { return objsToEnable; }
+            set { objsToEnable = value; }
+        }
 
-		/// Handle collision.
-		public void OnCollisionEnable(RaycastHit hitInfo) {
-		    var hitGOTag = hitInfo.transform.gameObject.tag;
+        private void FixedUpdate() {
+        }
 
-			foreach (ObjectToEnable obj in _objsToEnable) {
-				switch (obj.TagOption) {
-					case TagOptions.Include:
-						if (tag != obj.ExcludeTag) {
-							break;
-						}
-						obj.ObjToEnable.SetActive(true);
-						break;
-					case TagOptions.Exclude:
-						// Don't enable target object when hit a GO
-						// with excluded tag.
-						if (tag == obj.ExcludeTag) {
-							break;
-						}
-						obj.ObjToEnable.SetActive(true);
-						break;
-				}
-			}
-		}
-	}
+        /// Handle collision.
+        // todo refactor
+        public void OnCollisionEnable(RaycastHit hitInfo) {
+            var hitGOTag = hitInfo.transform.gameObject.tag;
+
+            foreach (ObjectToEnable obj in ObjsToEnable) {
+                switch (obj.TagOption) {
+                    case TagOptions.Include:
+                        if (tag != obj.ExcludeTag) {
+                            break;
+                        }
+                        obj.ObjToEnable.SetActive(true);
+                        break;
+                    case TagOptions.Exclude:
+                        // Don't enable target object when hit a GO
+                        // with excluded tag.
+                        if (tag == obj.ExcludeTag) {
+                            break;
+                        }
+                        obj.ObjToEnable.SetActive(true);
+                        break;
+                }
+            }
+        }
+    }
 }
